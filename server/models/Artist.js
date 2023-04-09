2241c59084989380986e8e6bf061ca8e9412b5a3
DelgaMatt <115049801+DelgaMatt@users.noreply.github.com>
2023-04-08 21:50:04 -0700

    added to typeDefs, resolvers, Artist.js, models/index.js

---
+++ server/models/Artist.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const artistSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'Must use a valid email address'],
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 5
+        }
+    }
+)
+
+// set up pre-save middleware to create password
+artistSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+});
+
+// compare the incoming password with the hashed password
+artistSchema.methods.isCorrectPassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+const Artist = model('Artist', artistSchema)
+
+module.exports = Artist;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,21 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+    type Query {
+        artists: [Artist]
+    }
+
+    type Artist {
+        _id: ID 
+        name: String,
+        email: String
+    }
+
+    type Mutation {
+        addArtist(name: String!, email: String!, password: String): Artist
+    }
+
+`;
+
+module.exports = typeDefs;
