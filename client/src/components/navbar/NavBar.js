b4ac9f7e2978c65c3e88b1c183af0f556b98756d
Sam Jensen <samual.jensen@gmail.com>
2023-04-12 21:01:33 -0700

    added buttons to Navbar.js

--- client/src/components/navbar/NavBar.js
+++ client/src/components/navbar/NavBar.js
@@ -4,24 +4,33 @@ import "./navbar.css";
 const Navbar = ({ handlePageChange }) => {
   return (
     <div className="navbar">
-      <h1>My App</h1>
+      <h1>Sherpa Records</h1>
       <nav>
         <ul className="header__nav-list">
           <li className="header__nav-item">
-            <button
-              className="header__nav-button"
-              onClick={() => handlePageChange("aboutUs")}
-            >
-              About Us
+            <button className="header__nav-button" onClick={() => handlePageChange("aboutUs")}>
+              Shop
             </button>
           </li>
           <li className="header__nav-item">
-            <button
-              className="header__nav-button"
-              onClick={() => handlePageChange("contactUs")}
-            >
+            <button className="header__nav-button" onClick={() => handlePageChange("contactUs")}>
+              Artists
+            </button>
+          </li>
+          <li className="header__nav-item">
+            <button className="header__nav-button" onClick={() => handlePageChange("contactUs")}>
+              About Us
+            </button>
+            <li className="header__nav-item">
+            <button className="header__nav-button" onClick={() => handlePageChange("contactUs")}>
               Contact Us
             </button>
+          </li>
+          <li className="header__nav-item">
+            <button className="header__nav-button" onClick={() => handlePageChange("contactUs")}>
+              Login/Sign Up
+            </button>
+          </li>
           </li>
         </ul>
       </nav>
