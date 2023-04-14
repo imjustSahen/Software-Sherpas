bb671cef9f0aaf1b5032ee4a45da8ffb7d62826c
wyamet <wyattdomanski.md@gmail.com>
2023-04-13 20:21:23 -0700

    updated description, added logo to crowd image and overlay to images

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Artist1 from "../assets/images/TFMarz-2.jpg";
 import Artist2 from "../assets/images/keramel-2.JPEG";
-import CrowdImage from "../assets/BGimages/crowdpic.png";
+import CrowdImage from "../assets/logos/CrowdLogo.png";
 import "./home.css";
 
 function HomePage() {
@@ -27,19 +27,19 @@ function HomePage() {
         </div>
         <div className="description-container">
           <p className="description">
-            Welcome to The Sherpa Studios, a music label dedicated to supporting
-            local artists based out of the Pacific Northwest. Our goal is to
-            provide a platform for talented musicians to showcase their work,
-            connect with new fans, and grow their careers. At The Sherpa
-            Studios, we believe in the power of music to bring people together
-            and inspire positive change. We are committed to fostering a vibrant
+            Welcome to The Sherpa Studios, a music label dedicated to carrying
+            local artists to the top. We're based out of the Pacific Northwest
+            and our mission is to provide a platform for talented musicians to
+            showcase their work, connect with new fans, and grow their careers.
+            We believe that music has the power to bring people together and
+            inspire positive change, and we're committed to fostering a vibrant
             and inclusive music community that celebrates diversity and
-            creativity. Whether you're a singer-songwriter, a rapper, or a
+            creativity. As a Sherpa, we'll be with you every step of the way on
+            your musical journey, providing guidance, support, and resources to
+            help you succeed. Whether you're a singer-songwriter, a rapper, or a
             producer, we invite you to join our community and share your unique
-            sound with the world. With a team of experienced professionals and a
-            passion for great music, we're here to help you succeed. Thank you
-            for visiting The Sherpa Studios. We can't wait to hear what you
-            create.
+            sound with the world. Thank you for visiting The Sherpa Studios. We
+            can't wait to hear what you create.
           </p>
         </div>
         <div className="featured-artist-container">
