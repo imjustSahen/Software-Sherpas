adfe0f13006cc1726dc94cefa356f2ca577f1015
imjustSahen <114109203+imjustSahen@users.noreply.github.com>
2023-04-17 16:34:51 -0700

    small Footer adjustmetns

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -1,40 +1,11 @@
 import React from "react";
-import "./footer.css";
+import "./Footer.css";
 
-const Footer = ({ handlePageChange }) => {
+const Footer = () => {
   return (
-    <div className="footer">
+    <footer>
       <h1>Sherpa Records</h1>
-      <nav>
-        <ul className="footer__nav-list">
-          <li className="footer__nav-item">
-            <button className="footer__nav-button" onClick={() => handlePageChange("shop")}>
-              Shop
-            </button>
-          </li>
-          <li className="footer__nav-item">
-            <button className="footer__nav-button" onClick={() => handlePageChange("artists")}>
-              Artists
-            </button>
-          </li>
-          <li className="footer__nav-item">
-            <button className="footer__nav-button" onClick={() => handlePageChange("aboutUs")}>
-              About
-            </button>
-            <li className="footer__nav-item">
-            <button className="footer__nav-button" onClick={() => handlePageChange("contactUs")}>
-              Contact
-            </button>
-          </li>
-          <li className="footer__nav-item">
-            <button className="footer__nav-button" onClick={() => handlePageChange("login")}>
-              Login
-            </button>
-          </li>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    </footer>
   );
 };
 
